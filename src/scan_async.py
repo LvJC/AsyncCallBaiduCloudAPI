87f66e1d82a811d60b25b4684757650042d071de
LvJC <lvjincheng@tju.edu.cn>
2021-05-07 15:02:57 +0800

    update AsyncScanner

--- src/scan_async.py
+++ src/scan_async.py
@@ -1,72 +1,123 @@
-import os
-import asyncio
-import logging
-import base64
-
-import glob
-import json
+import numpy as np
 import pandas as pd
 
-from utils import IMAGE_CENSOR, CLASS_DICT, read_file, request, urlencode, fetch_token
+from utils import read_image_file
+from aip_caller import BaiduAipCaller, CLASS_DICT, BD_LABEL_NAMES, BD_SUBLABEL_NAMES
 
-async def fetch_result(base_url, imgpath):
-    img = read_file(imgpath)
-    # logging.info("Send request")
-    loop = asyncio.get_event_loop()
-    future = loop.run_in_executor(None, request, base_url, urlencode({'image': base64.b64encode(img)}))
-    response = await future
-    # logging.info("Return response")
-    return imgpath, response
+class AsyncScanner(object):
+    def __init__(self, caller, image_path_list, label_dir, config) -> None:
+        self.caller = caller
+        self.image_path_list = image_path_list
+        self.label_dir = label_dir
+        self.config = config
+        self.raw_resp_csv_path = os.path.join(self.label_dir, "raw.csv")
 
-async def async_scan(base_url, input_path, output_path):
-    samples_dir_list = glob.glob(input_path+'*_samples/')
-    samples_dir_list.sort()
-    
-    samples_dir = samples_dir_list[0]
-    samples_path_list = []
-    from remove_corrupted_imgs import IMG_EXTENSIONS
-    for type in IMG_EXTENSIONS:
-        samples_path_list.extend(glob.glob(samples_dir+'**/*'+type, recursive=True))
-    samples_path_list = samples_path_list[:]
+    async def fetch_result(self, imgpath):
+        loop = asyncio.get_event_loop()
+        img = read_image_file(imgpath)
+        future = loop.run_in_executor(None, self.caller.predict, img) #img:bytes
+        response = await future
+        return imgpath, response
 
-    df = pd.DataFrame()
-    step_size = 40 #
-    n_samples = len(samples_path_list)
-    for start_ind in range(0, n_samples, step_size):
-        end_ind = start_ind + step_size
-        if end_ind >= n_samples:
-            end_ind = n_samples
-        logging.info(f"*** Processing {start_ind} - {end_ind}")
-        tasks = [ asyncio.create_task(fetch_result(base_url, path)) for path in samples_path_list[start_ind:end_ind] ]
-        await asyncio.wait(tasks)
-        result_list = [task.result() for task in tasks]
-        imgpath_list, resp_list = zip(*result_list)
-        for index in range(start_ind, end_ind):
-            df.loc[index, 'image_path'] = imgpath_list[index-start_ind]
+    async def async_scan(self):
+        """
+            Only save raw response message.
+        """
+        df = pd.DataFrame(columns=["image_path", "BDLabels"])
+        step_size = 50
+        n_samples = len(self.image_path_list)
+        for start_ind in range(0, n_samples, step_size):
+            end_ind = start_ind + step_size
+            if end_ind >= n_samples:
+                end_ind = n_samples
+            logging.info(f"*** Processing {start_ind} - {end_ind}")
+            tasks = [ asyncio.create_task(self.fetch_result(path)) for path in self.image_path_list[start_ind:end_ind] ]
+            await asyncio.wait(tasks)
+            result_list = [task.result() for task in tasks]
+            imgpath_list, resp_list = zip(*result_list)
+            for index in range(start_ind, end_ind):
+                df.loc[index, 'image_path'] = imgpath_list[index-start_ind]
+                try:
+                    data = resp_list[index-start_ind]
+                    df.loc[index, 'BDLabels'] = resp_list[index-start_ind] # json.dumps(data, ensure_ascii=False) if dict
+                except Exception as e:
+                    df.loc[index, 'BDLabels'] = f"{e}"
+                    logging.error("Error msg:", imgpath_list[index-start_ind], e)
+            df = self.export_results(df, self.raw_resp_csv_path)
+            
+    @staticmethod
+    def export_results(df, output_path):
+        (mode, header) = ('a',False) if os.path.exists(output_path) else ('w',True)
+        df.to_csv(output_path, mode=mode, header=header, encoding="utf_8_sig", index=False)
+        return df[0:0]
+
+    #TODO: Generate label score using sublabel scores
+    def postprocess(self):
+        sublabel_output_path = os.path.join(self.label_dir, "sublabel.csv")
+        self.postprocess_sublabel(sublabel_output_path)
+        # label_output_path = os.path.join(self.label_dir, "label.csv")
+        # self.postprocess_label(label_output_path)
+
+    def postprocess_sublabel(self, output_path):
+        df = pd.read_csv(self.raw_resp_csv_path, index_col=False)
+        sublabel_df = pd.DataFrame(columns=['image_path']+BD_SUBLABEL_NAMES)
+        for index in range(0, df.shape[0]):
+            sublabel_df['image_path'] = df['image_path']
             try:
-                data = json.loads(resp_list[index-start_ind])#["data"]
-                # logging.info(data,imgname)
-                df.loc[index, 'Labels'] = resp_list[index-start_ind] # json.dumps(data) # str
+                data = json.loads(df.loc[index, 'BDLabels'])
                 if data['conclusion'] != "合规":
                     for lable_dict in data['data']:
-                        df.loc[index, CLASS_DICT.get(lable_dict["msg"])] = lable_dict['probability']
+                        subclass_name = CLASS_DICT.get(lable_dict["msg"])
+                        sublabel_df.loc[index, subclass_name] = lable_dict['probability']
             except Exception as e:
-                df.loc[index, 'Labels'] = f"{e}"
-                logging.info("Error msg:", imgpath_list[index-start_ind], e)
-        df = export_results(df, output_path)
-        
+                logging.error(f"Error at index#{index}: {e}")
+                for sublabel_name in BD_SUBLABEL_NAMES:
+                    sublabel_df.loc[index, sublabel_name] = np.nan
 
-def export_results(df, output_path):
-    (mode, header) = ('a',False) if os.path.exists(output_path) else ('w',True)
-    df.to_csv(output_path, mode=mode, header=header, encoding="utf_8_sig")
-    return df[0:0] # reset df, reduce memory usage
+        sublabel_df.drop_duplicates().dropna(subset=[BD_SUBLABEL_NAMES[0]]).to_csv(output_path, encoding="utf_8_sig", index=False)
 
 
 if  __name__ == "__main__":
-    input_path = 'data/samples/nsfw_samples/'
-    output_path = 'results/nsfw_samples.csv'
-    token = fetch_token()
-    base_url = IMAGE_CENSOR + "?access_token=" + token
+    import argparse
+    from utils import load_config, get_io_info
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--input",
+        default="samples/nsfw_samples/",
+        help="path to input csv file (with column image_path) or data folder for inference",
+    )
+    parser.add_argument(
+        "--out_folder",
+        default="results/nsfw_samples/baidu/",
+        help="folder for logging and consolidated results"
+    )
+    parser.add_argument(
+        "--config_path",
+        default="conf/bd_config.yaml",
+        help="path to configuration file"
+    )
+
+    args = parser.parse_args()
+
+    # logging config
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s: - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S',
+                        level=logging.INFO)
+
+    config = load_config(args.config_path)
+    
+    input_dir = args.input
+    output_dir = args.out_folder
+
+    samples_path_list, label_dir = get_io_info(input_dir, output_dir)
+    
     for class_key in list(CLASS_DICT.keys()):
         CLASS_DICT["疑似存在"+class_key+"不合规"] = CLASS_DICT.pop(class_key)
-    asyncio.run( async_scan(base_url, input_path, output_path) ) 
+
+    bd_aip_caller = BaiduAipCaller(config['APP_ID'], config['API_KEY'], config['SECRET_KEY'])
+
+    scanner = AsyncScanner(bd_aip_caller, samples_path_list[:], label_dir, config)
+    asyncio.run( scanner.async_scan() )
+    logging.info("***** Done for raw response collection.")
+    scanner.postprocess()
